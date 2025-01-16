8a6746cf1695845bbbf210d0faab50683805fc92
mdsahbag-dev <mohdsahbag0786@gmail.com>
2025-01-16 19:08:19 +0530

    changes

--- RemoteSensingProject/Models/ProjectManager/ManagerService.cs
+++ RemoteSensingProject/Models/ProjectManager/ManagerService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using static RemoteSensingProject.Models.Admin.main;
 
 namespace RemoteSensingProject.Models.ProjectManager
 {
@@ -76,6 +78,107 @@ namespace RemoteSensingProject.Models.ProjectManager
             }
             return _details;
         }
-        #endregion 
+        #endregion
+
+        #region /* Add Project */
+        public bool addProject(createProjectModel pm)
+        {
+            con.Open();
+            SqlTransaction tran = con.BeginTransaction();
+            try
+            {
+                cmd = new SqlCommand("sp_adminAddproject", con, tran);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", "insertProject");
+                cmd.Parameters.AddWithValue("@title", pm.pm.ProjectTitle);
+                cmd.Parameters.AddWithValue("@assignDate", pm.pm.AssignDate);
+                cmd.Parameters.AddWithValue("@startDate", pm.pm.StartDate);
+                cmd.Parameters.AddWithValue("@completionDate", pm.pm.CompletionDate);
+                cmd.Parameters.AddWithValue("@projectmanager", pm.pm.ProjectManager);
+                cmd.Parameters.AddWithValue("@budget", pm.pm.ProjectBudget);
+                cmd.Parameters.AddWithValue("@description", pm.pm.ProjectDescription);
+                cmd.Parameters.AddWithValue("@ProjectDocument", pm.pm.projectDocumentUrl);
+                cmd.Parameters.AddWithValue("@projectType", pm.pm.ProjectType);
+                cmd.Parameters.AddWithValue("@stage", pm.pm.ProjectStage);
+                cmd.Parameters.AddWithValue("@createdBy", "admin");
+                cmd.Parameters.AddWithValue("@ApproveStatus", 1);
+                cmd.Parameters.Add("@project_Id", SqlDbType.Int);
+                cmd.Parameters["@project_Id"].Direction = ParameterDirection.Output;
+                int i = cmd.ExecuteNonQuery();
+                int projectId = Convert.ToInt32(cmd.Parameters["@project_Id"].Value != DBNull.Value ? cmd.Parameters["@project_Id"].Value : 0);
+                if (i > 0)
+                {
+                    if (pm.budgets != null && pm.budgets.Count > 0)
+                    {
+                        foreach (var item in pm.budgets)
+                        {
+                            cmd = new SqlCommand("sp_adminAddproject", con, tran);
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            cmd.Parameters.AddWithValue("@action", "insertProjectBudget");
+                            cmd.Parameters.AddWithValue("@project_Id", projectId);
+                            cmd.Parameters.AddWithValue("@heads", item.ProjectHeads);
+                            cmd.Parameters.AddWithValue("@headsAmount", item.ProjectAmount);
+                            cmd.Parameters.AddWithValue("@miscellaneous", item.Miscellaneous);
+                            cmd.Parameters.AddWithValue("@miscAmount", item.Miscell_amt);
+                            i += cmd.ExecuteNonQuery();
+                        }
+                    }
+                    if (pm.stages != null && pm.stages.Count > 0 && pm.pm.ProjectStage.Equals("Yes"))
+                    {
+                        foreach (var item in pm.stages)
+                        {
+                            cmd = new SqlCommand("sp_adminAddproject", con, tran);
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            cmd.Parameters.AddWithValue("@action", "insertProjectStatge");
+                            cmd.Parameters.AddWithValue("@project_Id", projectId);
+                            cmd.Parameters.AddWithValue("@keyPoint", item.KeyPoint);
+                            cmd.Parameters.AddWithValue("@completeDate", item.CompletionDate);
+                            cmd.Parameters.AddWithValue("@stageDocument", item.Document_Url);
+                            i += cmd.ExecuteNonQuery();
+                        }
+                    }
+
+                    if (pm.pm.ProjectType.Equals("External") && projectId > 0)
+                    {
+                        cmd = new SqlCommand("sp_adminAddproject", con, tran);
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@action", "insertExternalProject");
+                        cmd.Parameters.AddWithValue("@project_Id", projectId);
+                        cmd.Parameters.AddWithValue("@DepartmentName", pm.pm.ProjectDepartment);
+                        cmd.Parameters.AddWithValue("@contactPerson", pm.pm.ContactPerson);
+                        cmd.Parameters.AddWithValue("@address", pm.pm.Address);
+                        i += cmd.ExecuteNonQuery();
+                    }
+
+                    if (pm.pm.SubOrdinate.Length > 0)
+                    {
+                        foreach (var item in pm.pm.SubOrdinate)
+                        {
+                            cmd = new SqlCommand("sp_adminAddproject", con, tran);
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            cmd.Parameters.AddWithValue("@action", "insertSubOrdinate");
+                            cmd.Parameters.AddWithValue("@project_Id", projectId);
+                            cmd.Parameters.AddWithValue("@id", item);
+                            cmd.Parameters.AddWithValue("@projectmanager", pm.pm.ProjectManager);
+                            i += cmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+                tran.Commit();
+                return i > 0;
+            }
+            catch (Exception ex)
+            {
+                tran.Rollback();
+                return false;
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+                cmd.Dispose();
+            }
+        }
+        #endregion /* End */
     }
 }
