516c0c2d17668cb8a51184c52864768e9090103f
Rudra singh <rudrasingh400101@gmail.com>
2025-01-20 11:31:30 +0530

    changes

---
+++ RemoteSensingProject/Controllers/AccountsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RemoteSensingProject.Controllers
+{
+    [Authorize(Roles= "accounts")]
+    public class AccountsController : Controller
+    {
+        // GET: Accounts
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+       public ActionResult Requests()
+        {
+            return View();
+        } 
+        public ActionResult ApprovedList()
+        {
+            return View();
+        } 
+        public ActionResult RejectList()
+        {
+            return View();
+        }     
+        public ActionResult FundReport()
+        {
+            return View();
+        }    
+       
+    }
+}
