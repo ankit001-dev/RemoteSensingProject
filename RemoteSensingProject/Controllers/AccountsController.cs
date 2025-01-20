b1ed03d2fe6db62f30d3805958ec9abcd7dcbe5c
mdsahbag-dev <mohdsahbag0786@gmail.com>
2025-01-20 19:15:01 +0530

    all

--- RemoteSensingProject/Controllers/AccountsController.cs
+++ RemoteSensingProject/Controllers/AccountsController.cs
@@ -15,9 +15,9 @@ namespace RemoteSensingProject.Controllers
             return View();
         }
 
-       public ActionResult Requests()
+        public ActionResult Requests()
         {
-            return View();
+             return View();
         } 
         public ActionResult ApprovedList()
         {
