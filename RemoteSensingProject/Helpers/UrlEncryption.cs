e40fc6591b8d21ee22e1bc2a2b1b148f0302b050
Developer <Developer@DESKTOP-44NTE65>
2026-02-14 18:34:29 +0530

    some unuable packages deleted from the project. and project url encoding and decoding has been setup.

---
+++ RemoteSensingProject/Helpers/UrlEncryption.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace RemoteSensingProject.Helpers
+{
+    public static class UrlEncryption
+    {
+        private static readonly string key = "MacreelInf784280";
+        private static readonly string iv = "082487fnIleercaM";
+
+        public static string Encrypt(string plainText)
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = Encoding.UTF8.GetBytes(iv);
+
+                using (var encryptor = aes.CreateEncryptor())
+                {
+                    byte[] inputBytes = Encoding.UTF8.GetBytes(plainText);
+                    byte[] encrypted = encryptor.TransformFinalBlock(inputBytes, 0, inputBytes.Length);
+                    return Convert.ToBase64String(encrypted)
+                        .Replace("+", "-")
+                        .Replace("/", "_")
+                        .Replace("=", "");
+                }
+            }
+        }
+
+
+        public static string Decrypt(string cipherText)
+        {
+            cipherText = cipherText.Replace("-", "+").Replace("_", "/");
+            switch(cipherText.Length % 4)
+            {
+                case 2: cipherText += "=="; break;
+                case 3: cipherText += "="; break;
+            };
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = Encoding.UTF8.GetBytes (iv);
+
+                using (var decryptor = aes.CreateDecryptor())
+                {
+                    byte[] cipherBytes = Convert.FromBase64String(cipherText);
+                    byte[] decypted = decryptor.TransformFinalBlock(cipherBytes, 0, cipherBytes.Length);
+                    return Encoding.UTF8.GetString(decypted);
+                }
+            }
+        }
+    }
+}
---
+++ RemoteSensingProject/Helpers/UrlHelperExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RemoteSensingProject.Helpers
+{
+    public static class UrlHelperExtension
+    {
+        public static string EncryptedAction(this UrlHelper urlhelper, string action, string controller, object routeValues)
+        {
+            var Dict = new RouteValueDictionary(routeValues);
+            NameValueCollection queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            foreach(var item in Dict)
+            {
+                queryString[item.Key] = item.Value.ToString();
+            }
+
+            string plainQuery = queryString.ToString();
+            string encrypted = UrlEncryption.Encrypt(plainQuery);
+
+            return urlhelper.Action(action, controller, new { q = encrypted });
+        }
+    }
+}
